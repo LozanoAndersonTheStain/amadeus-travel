e70605813f20fbd72a1fa2d10f2a952133257e37
Anderson Lozano <adslozanov@gmail.com>
2025-07-03 22:55:13 -0500

    Begin a project. 🎉

---
+++ shared-vars.sh
@@ -0,0 +1,12 @@
+# shared-vars.sh
+
+# Sufijo compartido para nombres de recursos
+SUFFIX="aztro-amadeus"
+
+# Ubicación común para los recursos
+LOCATION="centralus"
+
+# Docker images and user
+API_IMAGE="aztro-api"
+WEB_IMAGE="aztro-web"
+DOCKERHUB_USER="lozanothestain" # Change this to your Docker Hub username
